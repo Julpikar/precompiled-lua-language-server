4fc9ce0ae398b95e6c3a128553abd1c01a2059ba
benawas <julpikargupran@gmail.com>
2020-12-11 19:18:58 +0700

    first commit

---
+++ script/core/matchkey.lua
@@ -0,0 +1,33 @@
+return function (me, other, fast)
+    if me == other then
+        return true
+    end
+    if me == '' then
+        return true
+    end
+    if #me > #other then
+        return false
+    end
+    local lMe = me:lower()
+    local lOther = other:lower()
+    if lMe == lOther:sub(1, #lMe) then
+        return true
+    end
+    if fast and me:sub(1, 1) ~= other:sub(1, 1) then
+        return false
+    end
+    local chars = {}
+    for i = 1, #lOther do
+        local c = lOther:sub(i, i)
+        chars[c] = (chars[c] or 0) + 1
+    end
+    for i = 1, #lMe do
+        local c = lMe:sub(i, i)
+        if chars[c] and chars[c] > 0 then
+            chars[c] = chars[c] - 1
+        else
+            return false
+        end
+    end
+    return true
+end
